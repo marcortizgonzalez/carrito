f26478f707f0484254157a329321d77b8a0c4d1b
Marc <marcortizgonzalez2002@gmail.com>
2022-02-18 18:16:30 +0100

    Login y logout

--- app/Http/Controllers/CamisetaController.php
+++ app/Http/Controllers/CamisetaController.php
@@ -19,79 +19,56 @@ class CamisetaController extends Controller
         $listaCamiseta = DB::table('tbl_camiseta')->select('*')->get();
         return view('principal', compact('listaCamiseta'));
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    
+    /*LogIn*/
+    public function login(){
+        return view('login');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function loginPost(Request $request){
+        //recogemos los datos, teniendo exepciones, como el token que utiliza laravel y el método
+        $datos = $request->except('_token', '_method');
+        //Validación datos por parte del server, es necesario aunque pase por JS 
+        // $request->validate([
+        //     'correo_usu' => 'required|string|max:60',
+        //     'pass_usu' => 'required|string|max:255'
+        // ]);
+
+        try {
+            //Hacemos la consulta con la DB, la cual contará nuestros resultados 1-0
+            $queryId = DB::table('tbl_usuario')->where('correo_usu', '=', $datos['correo_usu'])->where('pass_usu', '=', ($datos['pass_usu']))->count();
+            //Obtenemos todos los resultados de la DB, posteiriormente cogeremos un campo en concreto, obtenemos el primer resultado
+            $userId = DB::table('tbl_usuario')->where('correo_usu', '=', $datos['correo_usu'])->where('pass_usu', '=', ($datos['pass_usu']))->first();
+            //De los datos obtenidos consultamos el campo en concreto
+            $rol_usu = $userId->rol_usu;
+            //En caso de que la query $queryId devuelva como resultado 1(Coincidenci de datos haz...)
+            if ($queryId == 1){
+                //Establecemos sesión, solcitando el dato a Request
+                $request->session()->put('correo_usu', $request->correo_usu);
+                if($rol_usu == 'admin'){
+                    $request->session()->put('rol_usu', $rol_usu);
+                    return redirect("/principal_admin");
+                    //return $datos;
+                }else{
+                    $request->session()->put('rol_usu', $rol_usu);
+                    return redirect("/principal");
+                    //return $datos;
+                }
+            }else{
+                //No establecemos sesión y lo devolvemos a login
+                return redirect('/');
+            }
+        } catch (\Throwable $e) {
+            //En caso de error impredecible obtendremos el mismo error mediante $e
+            //return $e->getMessage();
+            return redirect('/');
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Camiseta  $camiseta
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Camiseta $camiseta)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Camiseta  $camiseta
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Camiseta $camiseta)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Camiseta  $camiseta
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Camiseta $camiseta)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Camiseta  $camiseta
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Camiseta $camiseta)
-    {
-        //
+    /*logout*/
+    public function logout(Request $request){
+        //Eliminar todas las variables de sesion
+        $request->session()->flush();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ use App\Http\Controllers\CamisetaController;
 |
 */
 
+/*LogIn y LogOut*/
+Route::get('',[CamisetaController::class, 'login']);
+
+Route::post('login',[CamisetaController::class, 'loginPost']);
+
+Route::get('logout',[CamisetaController::class, 'logout']);
+
 /*Mostrar*/
 Route::get('/principal',[CamisetaController::class, 'mostrarCamiseta']);
 
