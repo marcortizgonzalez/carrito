a728e7d798085e01fa0f175782f5f4a814363205
Marc <marcortizgonzalez2002@gmail.com>
2022-02-18 16:03:22 +0100

    first commit

---
+++ app/Http/Controllers/CamisetaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Camiseta;
+use Illuminate\Http\Request;
+
+class CamisetaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Camiseta  $camiseta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Camiseta $camiseta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Camiseta  $camiseta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Camiseta $camiseta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Camiseta  $camiseta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Camiseta $camiseta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Camiseta  $camiseta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Camiseta $camiseta)
+    {
+        //
+    }
+}
